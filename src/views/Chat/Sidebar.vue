fcfcee0151ac84a719c691372ed0e9a499f8425b
Xiao Yav1 <1409951865@qq.com>
2024-12-26 15:31:16 +0800

    refactor(Chat): 重构侧边栏组件
    - 移除多余的代码和样式
    - 引入新的 SidebarMain 组件
    - 删除了不再使用的图标组件

--- src/views/Chat/Sidebar.vue
+++ src/views/Chat/Sidebar.vue
@@ -13,40 +13,7 @@
     </div>
 
     <!-- main -->
-
-    <ul class="flex-1 overflow-auto mt-5">
-      <li class="menu-title">今天</li>
-      <!-- 记录 -->
-      <div class="dropdown dropdown-bottom dropdown-end w-full" v-for="i in 2">
-        <li class="group">
-          <a :class="{ focus: i === 1 }">
-            <span class="text-ellipsis"
-              >爱是自由意志的沉沦爱是自由意志的沉沦</span
-            >
-            <button
-              :tabindex="0"
-              class="more-drop"
-              data-tip="更多操作"
-              @click.stop
-            >
-              <MoreIcon />
-            </button>
-          </a>
-        </li>
-        <!-- 下拉菜单内容 -->
-        <ul
-          tabindex="0"
-          class="dropdown-content menu bg-base-100 rounded-box z-[1] w-max p-2 shadow"
-        >
-          <li v-for="item in editData" :key="item.name">
-            <a :class="{ 'text-error': item.name === '删除' }">
-              <component :is="item.icon" width="18" />
-              {{ item.name }}</a
-            >
-          </li>
-        </ul>
-      </div>
-    </ul>
+    <SidebarMain />
 
     <!-- footer -->
     <div class="border-t-[1px] border-base-300">
@@ -57,31 +24,15 @@
 </template>
 
 <script setup lang="ts">
+// 组件
+import SidebarMain from "./SidebarMain.vue";
 // 图标
 import EditIcon from "@/assets/svg/edit.svg?component";
-import MoreIcon from "@/assets/svg/more.svg?component";
-import DeleteIcon from "@/assets/svg/delete.svg?component";
-import PenIcon from "@/assets/svg/pen.svg?component";
 
 /** 创建新对话 */
 const createNewChat = () => {
   console.log("createNewChat");
 };
-
-/** 操作数据 */
-const editData = [
-  { name: "删除", icon: DeleteIcon },
-  { name: "重命名", icon: PenIcon },
-];
 </script>
 
-<style scoped>
-.text-ellipsis {
-  /*1. 先强制一行内显示文本*/
-  white-space: nowrap;
-  /*2. 超出的部分隐藏*/
-  overflow: hidden;
-  /*3. 文字用省略号替代超出的部分*/
-  text-overflow: ellipsis;
-}
-</style>
+<style scoped></style>
---
+++ src/views/Chat/SidebarMain.vue
@@ -0,0 +1,58 @@
+<script setup lang="ts">
+import MoreIcon from "@/assets/svg/more.svg?component";
+import DeleteIcon from "@/assets/svg/delete.svg?component";
+import PenIcon from "@/assets/svg/pen.svg?component";
+
+/** 操作数据 */
+const editData = [
+  { name: "删除", icon: DeleteIcon },
+  { name: "重命名", icon: PenIcon },
+];
+</script>
+
+<template>
+  <ul class="flex-1 overflow-auto mt-5">
+    <li class="menu-title">今天</li>
+    <!-- 记录 -->
+    <div class="dropdown dropdown-bottom dropdown-end w-full" v-for="i in 2">
+      <li class="group">
+        <a :class="{ focus: i === 1 }">
+          <span class="text-ellipsis"
+            >爱是自由意志的沉沦爱是自由意志的沉沦</span
+          >
+          <button
+            :tabindex="0"
+            class="more-drop"
+            data-tip="更多操作"
+            @click.stop
+          >
+            <MoreIcon />
+          </button>
+        </a>
+      </li>
+      <!-- 下拉菜单内容 -->
+      <ul
+        tabindex="0"
+        class="dropdown-content menu bg-base-100 rounded-box z-[1] w-max p-2 shadow"
+      >
+        <li v-for="item in editData" :key="item.name">
+          <a :class="{ 'text-error': item.name === '删除' }">
+            <component :is="item.icon" width="18" />
+            {{ item.name }}</a
+          >
+        </li>
+      </ul>
+    </div>
+  </ul>
+</template>
+
+<style scoped>
+.text-ellipsis {
+  /*1. 先强制一行内显示文本*/
+  white-space: nowrap;
+  /*2. 超出的部分隐藏*/
+  overflow: hidden;
+  /*3. 文字用省略号替代超出的部分*/
+  text-overflow: ellipsis;
+}
+</style>
