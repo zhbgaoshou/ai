0bab173e50533554f781eff2ac2be8bc38735547
Xiao Yav2 <1409951865@qq.com>
2024-12-27 00:40:16 +0800

    refactor(Chat): 优化侧边栏组件结构
    - 移除了不必要的 div 包装层
    - 删除了未使用的 toggleSidebar 函数
    - 简化了侧边栏的 HTML 结构，提高了代码可读性
    - 减少了 CSS 类的使用，降低了样式复杂度

--- src/views/Chat/Sidebar.vue
+++ src/views/Chat/Sidebar.vue
@@ -1,43 +1,24 @@
 <template>
-  <div class="relative h-full">
-    <section
-      class="menu bg-base-200 justify-between min-h-0 h-full overflow-hidden p-0 transition-all"
-      :class="sideWidth"
-    >
-      <!-- header -->
-      <div class="m-2">
-        <li>
-          <a class="flex justify-between gap-2">
-            创建新对话
-            <div class="tooltip tooltip-left" data-tip="创建对话">
-              <EditIcon width="18" @click.stop="createNewChat" />
-            </div>
-          </a>
-        </li>
-      </div>
-
-      <!-- main -->
-      <SidebarMain class="p-4" />
-
-      <!-- footer -->
-      <div class="border-t-[1px] border-base-300 m-2">
-        <li class="mt-2"><a>升级</a></li>
-        <li class="mt-2"><a>签到</a></li>
-      </div>
-    </section>
-
-    <div
-      class="tooltip tooltip-right absolute top-[40px] -right-4"
-      data-tip="伸缩"
-    >
-      <button
-        class="btn btn-circle btn-sm border-base-100"
-        @click="toggleSidebar"
-      >
-        <CodeIcon width="18" />
-      </button>
+  <section class="menu w-80 bg-base-200 justify-between min-h-0 h-full overflow-hidden p-0 transition-all">
+    <!-- header -->
+    <div class="m-2">
+      <li>
+        <a class="flex justify-between gap-2">
+          创建新对话
+          <div class="tooltip tooltip-left" data-tip="创建对话">
+            <EditIcon width="18" @click.stop="createNewChat" />
+          </div>
+        </a>
+      </li>
     </div>
-  </div>
+    <!-- main -->
+    <SidebarMain class="p-4" />
+    <!-- footer -->
+    <div class="border-t-[1px] border-base-300 m-2">
+      <li class="mt-2"><a>升级</a></li>
+      <li class="mt-2"><a>签到</a></li>
+    </div>
+  </section>
 </template>
 
 <script setup lang="ts">
@@ -53,11 +34,7 @@ import CodeIcon from "@/assets/svg/code.svg?component";
 const createNewChat = () => {
   console.log("createNewChat");
 };
-/** 侧边栏 展开/收起 */
-let sideWidth = ref("w-64");
-function toggleSidebar() {
-  sideWidth.value = sideWidth.value === "w-64" ? "w-0" : "w-64";
-}
+
 </script>
 
 <style scoped></style>
